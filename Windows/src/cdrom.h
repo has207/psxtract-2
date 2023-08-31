a212d47dd9f7c97c9e666d20665647a7898ca7d0
Heel <heel@example.com>
2023-08-31 23:32:31 +0900

    Merge latest isofix changes and revert useless formatting diffs to make pulling easier in the future.

--- Windows/src/cdrom.h
+++ Windows/src/cdrom.h
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Position and size of individual elements in a CD sector.
+//Position and size of individual elements in a CD sector.
 #define SECTOR_SIZE                    2352
 #define BOOTLOADER_SECTORS             16
 #define BOOTLOADER_SIZE                (BOOTLOADER_SECTORS * SECTOR_SIZE)
@@ -36,7 +36,7 @@
 #define CDROMXA_FORM1_PARITY_Q_OFFSET  (CDROMXA_FORM1_PARITY_P_OFFSET + CDROMXA_FORM1_PARITY_P_SIZE)
 #define CDROMXA_FORM1_PARITY_Q_SIZE    (26 * 2 * 2)
 
-// The following table is used for computing the error detection code (EDC).
+//The following table is used for computing the error detection code (EDC)
 static unsigned int EDCTable[256] =
 {
     /*          0           1           2           3           4           5           6           7           8           9           A           B           C           D           E           F            */
@@ -59,7 +59,7 @@ static unsigned int EDCTable[256] =
     /*          0           1           2           3           4           5           6           7           8           9           A           B           C           D           E           F            */
 };
 
-// The following table is used for computing the error correction code (ECC).
+//The following table is used for computing the error correction code (ECC)
 static unsigned short RSPCTable[43][256] =
 {
     /*          00      01      02      03      04      05      06      07      08      09      0A      0B      0C      0D      0E      0F      10      11      12      13      14      15      16      17      18      19      1A      1B      1C      1D      1E      1F      20      21      22      23      24      25      26      27      28      29      2A      2B      2C      2D      2E      2F      30      31      32      33      34      35      36      37      38      39      3A      3B      3C      3D      3E      3F      40      41      42      43      44      45      46      47      48      49      4A      4B      4C      4D      4E      4F      50      51      52      53      54      55      56      57      58      59      5A      5B      5C      5D      5E      5F      60      61      62      63      64      65      66      67      68      69      6A      6B      6C      6D      6E      6F      70      71      72      73      74      75      76      77      78      79      7A      7B      7C      7D      7E      7F      80      81      82      83      84      85      86      87      88      89      8A      8B      8C      8D      8E      8F      90      91      92      93      94      95      96      97      98      99      9A      9B      9C      9D      9E      9F      A0      A1      A2      A3      A4      A5      A6      A7      A8      A9      AA      AB      AC      AD      AE      AF      B0      B1      B2      B3      B4      B5      B6      B7      B8      B9      BA      BB      BC      BD      BE      BF      C0      C1      C2      C3      C4      C5      C6      C7      C8      C9      CA      CB      CC      CD      CE      CF      D0      D1      D2      D3      D4      D5      D6      D7      D8      D9      DA      DB      DC      DD      DE      DF      E0      E1      E2      E3      E4      E5      E6      E7      E8      E9      EA      EB      EC      ED      EE      EF      F0      F1      F2      F3      F4      F5      F6      F7      F8      F9      FA      FB      FC      FD      FE      FF           */
@@ -109,14 +109,14 @@ static unsigned short RSPCTable[43][256] =
     /*          00      01      02      03      04      05      06      07      08      09      0A      0B      0C      0D      0E      0F      10      11      12      13      14      15      16      17      18      19      1A      1B      1C      1D      1E      1F      20      21      22      23      24      25      26      27      28      29      2A      2B      2C      2D      2E      2F      30      31      32      33      34      35      36      37      38      39      3A      3B      3C      3D      3E      3F      40      41      42      43      44      45      46      47      48      49      4A      4B      4C      4D      4E      4F      50      51      52      53      54      55      56      57      58      59      5A      5B      5C      5D      5E      5F      60      61      62      63      64      65      66      67      68      69      6A      6B      6C      6D      6E      6F      70      71      72      73      74      75      76      77      78      79      7A      7B      7C      7D      7E      7F      80      81      82      83      84      85      86      87      88      89      8A      8B      8C      8D      8E      8F      90      91      92      93      94      95      96      97      98      99      9A      9B      9C      9D      9E      9F      A0      A1      A2      A3      A4      A5      A6      A7      A8      A9      AA      AB      AC      AD      AE      AF      B0      B1      B2      B3      B4      B5      B6      B7      B8      B9      BA      BB      BC      BD      BE      BF      C0      C1      C2      C3      C4      C5      C6      C7      C8      C9      CA      CB      CC      CD      CE      CF      D0      D1      D2      D3      D4      D5      D6      D7      D8      D9      DA      DB      DC      DD      DE      DF      E0      E1      E2      E3      E4      E5      E6      E7      E8      E9      EA      EB      EC      ED      EE      EF      F0      F1      F2      F3      F4      F5      F6      F7      F8      F9      FA      FB      FC      FD      FE      FF           */
 };
 
-// Internal error codes.
+//Opens the input image, fixes it sector by sector and writes the fixed sectors to the output file
 #define ERROR_OUT_OF_MEMORY     1
 #define ERROR_INPUT_IO_ERROR    2
 #define ERROR_OUTPUT_IO_ERROR   3
 #define ERROR_IMAGE_INCOMPLETE  4
 #define ERROR_UNEXPECTED_MODE   5
 #define ERROR_UNSUPPORTED_MODE  6
-#define ERROR_SUBHEADER_DAMAGED 7
+#define ERROR_SUBHEADER_DAMAGED 7 //No longer used
 #define ERROR_MODE0_IS_NOT_0    8
 
 // EDC calculation mode.
@@ -131,13 +131,22 @@ enum EDCMode
 // Internal image fixing status structure.
 struct fixImageStatus
 {
-    int  errorcode;
-    int  mode0sectors;
-    int  mode1sectors;
-    int  mode2form1sectors;
-    int  mode2form2sectors;
-    int  form2bootsectorswithedc;
-    int  form2bootsectorswithoutedc;
+    int errorcode;
+    int mode0sectors;
+    int mode1sectors;
+    int mode2form1sectors;
+    int mode2form2sectors;
+    int form2bootsectorswithedc;
+    int form2bootsectorswithoutedc;
+    char** warnings;
+    unsigned int warningscount;
 };
+
+#define addWarning(status, formatString, maxLength, ...) \
+++status.warningscount;\
+status.warnings = (char **)realloc(status.warnings, sizeof(status.warnings) * status.warningscount);\
+char* warningString = (char *)malloc(maxLength);\
+snprintf(warningString, maxLength, formatString, __VA_ARGS__);\
+status.warnings[status.warningscount - 1] = warningString;\
 
 int make_cdrom(char* inputfile, char* outputfile, bool verbose);
